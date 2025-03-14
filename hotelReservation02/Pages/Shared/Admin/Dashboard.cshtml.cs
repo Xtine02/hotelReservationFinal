bb8a53490d54768ad31062cc391d7fa84c958ec1
JohnCliford02 <chriford02@gmail.com>
2025-03-14 20:43:18 +0800

    "updated frontend"

---
+++ hotelReservation02/Pages/Shared/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hotelReservation02.Pages.Admin
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
