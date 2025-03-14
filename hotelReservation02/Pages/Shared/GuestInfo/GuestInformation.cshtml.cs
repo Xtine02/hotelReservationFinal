76f5cddea68a24658102244dc85c6728677332bd
Dave Brian Redillas <davebrianredillas742@gmail.com>
2025-03-14 21:18:52 +0800

    Added Guest Information Management UI and Controller

---
+++ hotelReservation02/Pages/Shared/GuestInfo/GuestInformation.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace hotelReservation02.Pages.Admin
+{
+    public class GuestInformationModel : PageModel
+    {
+        public List<GuestInfo> Guests { get; set; } = new List<GuestInfo>();
+
+        public void OnGet()
+        {
+            // TODO: Replace this with actual data from your database
+            Guests = new List<GuestInfo>
+            {
+                new GuestInfo { RoomNumber = "0001", CustomerName = "John Smith", Mobile = "09123456789", Email = "johnsmith@gmail.com", RoomType = "DELUXE ROOM" },
+                new GuestInfo { RoomNumber = "0002", CustomerName = "Jane Doe", Mobile = "09213456789", Email = "janedoe@gmail.com", RoomType = "DELUXE ROOM" }
+            };
+        }
+
+        public class GuestInfo
+        {
+            public string RoomNumber { get; set; }
+            public string CustomerName { get; set; }
+            public string Mobile { get; set; }
+            public string Email { get; set; }
+            public string RoomType { get; set; }
+        }
+    }
